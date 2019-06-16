c02bd94771331a30695d182c577017bc6e6ac12e
Susanne Lundkvist <susanne.r.lundkvist@gmail.com>
2019-06-16 14:14:12 +0100

    api returning title and pictures

--- src/App.js
+++ src/App.js
@@ -7,14 +7,18 @@ const API_KEY = "e7851e20db27718422053cee2758743f"
 
 class App extends Component {
 
+  state = {
+    recipes: []
+  }
+
   getRecipe = async (e) => {
     const recipeName = e.target.elements.recipeName.value
     e.preventDefault()
-    const api_call = await fetch(`https://www.food2fork.com/api/search?key=${API_KEY}&q=shredded%20chicken`)
+    const api_call = await fetch(`https://www.food2fork.com/api/search?key=${API_KEY}&q=${recipeName}&count=10`)
 
     const data = await api_call.json()
-    console.log(data)
-
+    this.setState({ recipes: data.recipes })
+    console.log(this.state.recipes)
   }
 
   render() {
@@ -24,6 +28,14 @@ class App extends Component {
         <h1 className="App-title">Recipe Search</h1>
       </header>
       <Form getRecipe={this.getRecipe}/>
+      { this.state.recipes.map((recipe) => {
+        return (
+          <div key={recipe.recipe_id}>
+            <img src={recipe.image_url} alt={recipe.title}/>
+            <p >{ recipe.title }</p>
+          </div>
+        )
+      })}
     </div>
   );
 }
