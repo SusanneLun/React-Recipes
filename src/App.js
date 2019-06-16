0ce960fe4420896565c0d7926188a1235b1fb84b
Susanne Lundkvist <susanne.r.lundkvist@gmail.com>
2019-06-16 13:30:24 +0100

    search field and button added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
+import Form from './Form'
 
 class App extends Component {
+
+  getRecipe = (e) => {
+    e.preventDefault()
+    console.log("working")
+  }
   render() {
   return (
     <div className="App">
       <header className="App-header">
         <h1 className="App-title">Recipe Search</h1>
       </header>
+      <Form getRecipe={this.getRecipe}/>
     </div>
   );
 }
