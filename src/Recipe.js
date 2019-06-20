f3bb71c19f86ecaf34cd21bf5ed49db76d8dcbce
Susanne Lundkvist <susanne.r.lundkvist@gmail.com>
2019-06-20 15:51:09 +0100

    router introduced and recipe page

---
+++ src/Recipe.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class Recipe extends Component {
+render() {
+  console.log(this.props)
+  return (
+    <div>Recipe Component</div>
+
+  )
+}
+}
+
+
+
+export default Recipe
