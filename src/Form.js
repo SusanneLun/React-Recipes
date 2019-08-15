96cf311f60ef63e17438418def6200ec79b83505
Susanne Lundkvist <susanne.r.lundkvist@gmail.com>
2019-08-15 11:12:37 +0100

    security vulnerabilities fixed

--- src/Form.js
+++ src/Form.js
@@ -1,13 +1,21 @@
 import React from 'react'
 
 
-const Form = props => {
-  return (
+const Form = props => (
+
+<div className="container">
+  <div className="row">
   <form onSubmit={props.getRecipe} style={{ marginBottom:"2rem"}}>
     <input className="form__input" type="text" name="recipeName"/>
     <button className="form__button">Search</button>
   </form>
+
+  </div>
+</div>
+
+
+
 )
-}
+
 
 export default Form
--- src/Recipe.js
+++ src/Recipe.js
@@ -11,9 +11,7 @@ class Recipe extends Component {
 
   componentDidMount = async ()=> {
     const title = this.props.location.state.recipe
-    const req = await fetch
-    (`https://www.food2fork.com/api/search?key=${API_KEY}&q=${title}`)
-
+    const req = await fetch(`https://www.food2fork.com/api/search?key=${API_KEY}&q=${title}`)
     const res = await req.json()
     this.setState({ activeRecipe: res.recipes[0]})
     console.log(this.state.activeRecipe)
